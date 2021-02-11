f379e64051205ff059844c9835a0f8cf6bb7f74a
Eduardo Portella <eportella70@gmail.com>
2021-02-11 19:47:04 -0300

    JOGO DESGRACADO

--- C/jogoDigital/jogoDigital.cpp
+++ C/jogoDigital/jogoDigital.cpp
@@ -1,11 +1,44 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#define M 8  // pares de cartas
-#define n 4  // tabuleiro 4x4 que armazena 16 cartas
-
+#define M 18  // pares de cartas
+#define n 6  // tabuleiro 4x4 que armazena 16 cartas
 char tab[n][n];
-char cartas[M] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+char cartas[M] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'};
+
+float **alocaMat(int N){
+    float **Mat;
+    int i;
+    Mat = (float **) malloc(N*sizeof(float *));
+    if (Mat == NULL){
+        printf("Erro ao alocar memoria \n");
+        exit(-1);
+    }
+    for ( i = 0; i < N; i++)
+    {
+        Mat[i] = (float *) malloc(N*sizeof(float));
+        if (Mat[i] == NULL){
+            printf("Erro ao alocar memoria\n");
+            exit(-2);
+        }
+    }
+    printf("Matriz alocada com sucesso \n");
+    return Mat;
+}
+
+void dificuldade(){
+	int dif, i, j, cont, x;
+	printf("Escolha a difuldade:\n 1) Normal 2) Dificil\n");
+	scanf("%d", &dif);
+	if (dif != 1 && dif != 2){
+		printf("ERRO");
+		exit(-1);
+	} else if (dif == 1){
+		printf("OK!");
+	} else {
+		printf("OK!");
+	}
+}
 
 void criaTab() {
 	int i, j, cont, x;
@@ -17,8 +50,8 @@ void criaTab() {
     x=0;
     cont=0;
 	while (x<M){  // para cada elemento em V
-		i=rand() % 4;
-		j=rand() % 4;
+		i=rand() % n;
+		j=rand() % n;
 		if (tab[i][j] == 'Z') {
 		   tab[i][j] = cartas[x];
 		   cont++;
@@ -41,8 +74,12 @@ void mostra_tab() {
 }
 
 int main() {
+	float **MA;
+	dificuldade();
+	MA = alocaMat(n);
 	criaTab();
 	mostra_tab();
-    return 0;	
+	system("pause");
+    return 0;
 }
 
