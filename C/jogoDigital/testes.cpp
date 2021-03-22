ae9c18fb278fd8929589129c6bcf6230f2dcde12
Eduardo Portella <eportella70@gmail.com>
2021-03-22 11:54:43 -0300

    salvando textos

--- C/jogoDigital/testes.cpp
+++ C/jogoDigital/testes.cpp
@@ -1,12 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
 
 int main(){
-    int n;
-    char a='.';
-    printf("%d\n", isdigit(n));
-    printf("%d", isdigit(a));
+    int pontos;
+    char nome;
+    FILE *file;
+    file = fopen("teste.txt", "a");
+    printf("NOME: ");
+    gets(&nome);
+    printf("PONTOS: ");
+    scanf("%d", &pontos);
+    fprintf(file, "%c   %d\n", nome, pontos);
+    fclose(file);
+
     system("pause");
     return 0;
 }
