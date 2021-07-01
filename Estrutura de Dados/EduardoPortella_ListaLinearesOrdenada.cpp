cd0eff3f93b5658b45d63db3adc21aa6059f27f2
Eduardo Portella <eportella70@gmail.com>
2021-06-30 21:36:40 -0300

    lista linear ordenada

---
+++ Estrutura de Dados/EduardoPortella_ListaLinearesOrdenada.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+const int TAM=6;
+int lista[TAM];
+int tamanho=0;
+
+void insereLista(int valor){
+    int i;
+    if (tamanho==TAM){
+        cout << "Lista cheia " << endl;
+        return;
+    }
+    for (i=tamanho; i>0 && valor<lista[i-1]; i--){
+        lista[i]=lista[i-1];
+    }
+    lista[i] = valor;
+    tamanho++;
+    cout << "Elemento inserido" << endl;
+    return;
+}
+
+void removeLista(int valor){
+    int i, j;
+    for (i = 0; i < tamanho; i++){
+        if (lista[i] == valor){
+            for (j = i; tamanho--; j++){
+                lista[j]=lista[j+1];
+            }
+            tamanho--;
+            cout << "Elemento removido" << endl;
+            return;
+        }
+    }
+    cout << "Elemento nao encontrado" << endl;
+}
+
+void imprime(){
+    int i;
+    for (i=0; i<tamanho; i++){
+        cout << lista[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    insereLista(9);
+    insereLista(16);
+    insereLista(5);
+    insereLista(11);
+    imprime();
+    removeLista(16);
+    removeLista(8);
+    imprime();
+
+    system("pause");
+    return 0;
+}
