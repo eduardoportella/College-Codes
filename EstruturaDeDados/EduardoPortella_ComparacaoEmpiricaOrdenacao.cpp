ef4069f32b54702a724445931de7b56b226c1b2e
Eduardo Portella <eportella70@gmail.com>
2021-08-16 19:29:12 -0300

    ordenacao INCOMPLETO

--- EstruturaDeDados/EduardoPortella_ComparacaoEmpiricaOrdenacao.cpp
+++ EstruturaDeDados/EduardoPortella_ComparacaoEmpiricaOrdenacao.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const int TAM = 40000;
+const int TAM = 10;
 
 int vetor[TAM];
 
@@ -17,6 +17,34 @@ void gera(){
     for ( i = 0; i < TAM; i++)
     {
         vetor[i] = rand();
+    }
+}
+
+int PARTITION(int p, int r){
+    int piv = vetor[p];
+    int i = p-1, j = r+1, temp;
+    while (true){
+        j--;
+        while (vetor[j] > piv){
+            i++;
+        } while (vetor[i] < piv){
+            if (i<j){
+                temp = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = temp;
+            } else {
+                return j;
+            }
+        }
+    }
+}
+
+void quickSort(int p, int r){
+    int q;
+    if (p<r){
+        q = PARTITION(p, r);
+        quickSort(p, q);
+        quickSort(q+1, r);
     }
 }
 
@@ -39,6 +67,43 @@ void bubbleSort(){
     }
 }
 
+void selecao(){
+    int i, corrente, k, temp;
+    for (i=0; i<=TAM-2; i++){
+        corrente = i;
+        for (k=i+1; k<=TAM-1; k++){
+            if (vetor[corrente] > vetor[k]){
+                corrente = k;
+            }
+            temp = vetor[i];
+            vetor[i] = vetor[corrente];
+            vetor[corrente] = temp;
+        }
+    }
+}
+
+void mergeSort(int lo, int hi){
+    int media, L, H, scratch[TAM];
+    if (lo<hi){
+        media = (lo+hi)/2;
+        mergeSort(lo, media);
+        mergeSort(media+1, hi);
+        L = lo;
+        H = media+1;
+        for (int k=lo; k<hi; k++){
+            if (L <= media && (H>hi || vetor[L] < vetor[H])){
+                scratch[k] = vetor[L];
+                L++;
+            } else {
+                scratch[k] = vetor[H];
+                H++;
+            }
+        } for (int k=lo; k<hi; k++){
+            vetor[k] = scratch[k];
+        }
+    }
+}
+
 void imprime(){
     int i;
     for ( i = 0; i < TAM; i++)
@@ -50,10 +115,16 @@ void imprime(){
 
 int main(){
     gera();
-    // imprime();
+    imprime();
     tempo1 = clock();
-    bubbleSort();
+    // bubbleSort(); //FUNCIONANDO
+    quickSort(0, TAM-1);
+    // selecao(); // FUNCIONANDO
+    // mergeSort(0, TAM-1); //FUNCIONANDO
     tempo2 = clock() - tempo1;
     cout << "Tempo: " << (float) tempo2/CLOCKS_PER_SEC << " s" << endl;
-    // imprime();
+    imprime();
+
+    system("pause");
+    return 0;
 }
